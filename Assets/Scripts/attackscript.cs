2ffd38338b75246e6bfbc3606ce80eabbc4404ff
hantulin2@mail.ru <hantulin2@mail.ru>
2023-03-08 16:03:33 +0300

    Добавил интеракцию попытка #2

--- Assets/Scripts/attackscript.cs
+++ Assets/Scripts/attackscript.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class attackscript : MonoBehaviour
 {
     Animator animator;
-    public Collider sword;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        sword.enabled = false;
     }
 
     // Update is called once per frame
@@ -19,12 +17,10 @@ public class attackscript : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             animator.SetBool("attack", true);
-            sword.enabled = true;
 
         }
         else if (Input.GetButtonUp("Fire1"))
         {
-            sword.enabled = true;
             animator.SetBool("attack", false);
         }
     }
