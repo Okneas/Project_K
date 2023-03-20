eb0ac0ad8b3d459739d66ec96531d593e0db59fb
hantulin2@mail.ru <hantulin2@mail.ru>
2023-03-20 16:03:01 +0300

    Добавил сохранения (пока не очень, но в целом пойдёт)

--- Assets/Scripts/ButtonExit.cs
+++ Assets/Scripts/ButtonExit.cs
@@ -14,6 +14,9 @@ public class ButtonExit : MonoBehaviour
     }
     public void NewGame(int _scene)
     {
+        PlayerPrefs.SetFloat("PosX", 0);
+        PlayerPrefs.SetFloat("PosZ", 0);
+        PlayerPrefs.SetFloat("PosY", 0);
         SceneManager.LoadScene(_scene); 
     }
 }
